f2e65db1ce19f7de4b3352e9730569f694f4196d
tehedor <sergiot1805@gmail.com>
2024-06-19 16:09:13 +0200

    python

--- scripts/simulatorDocker/iniciarSimulator.py
+++ scripts/simulatorDocker/iniciarSimulator.py
@@ -4,6 +4,6 @@ Number = '001'
 
 workdir = "scripts/simulatorDocker"
 
-subprocess.run(['python', f'{workdir}/iniciarEntidades2.py', Number])
-subprocess.run(['python', f'{workdir}/subscribeActuators.py', Number])
-subprocess.run(['python', f'{workdir}/subscribeDraco.py', Number])
+subprocess.run(['python3', f'{workdir}/iniciarEntidades2.py', Number])
+subprocess.run(['python3', f'{workdir}/subscribeActuators.py', Number])
+subprocess.run(['python3', f'{workdir}/subscribeDraco.py', Number])
